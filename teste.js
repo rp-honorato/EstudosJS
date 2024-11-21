32431a86413ab3d9656afcb07a5ff9ae73da78c1
rp-honorato <rpedro.honorato@gmail.com>
2024-11-21 14:46:11 +0000

    Async Function

--- teste.js
+++ teste.js
@@ -1,21 +1,33 @@
-const URL = "https://reqres.in/api/users/";
+ // const URL = "https://reqres.in/api/users/";
 
-fetch(URL, {
+ // fetch(URL, {
     //método
-    method: "POST", 
+ //     method: "POST", 
     //Cabeçalho JSON
-    headers: {
-    "Content-Type": 'application/JSON'
-    },
-    body: JSON.stringify({
-        name: "Pedro",
-        age: "26"
-    })
-})
-.then(response => response.json())//Converte a resposta da requisição para JSON
-.then(data => {
-console.log(data) //Exibe os Dados
-    })
-.catch(error => {
-console.log(error, "Erro!") //Exibe erros
-});
+ //     headers: {
+ //     "Content-Type": 'application/JSON'
+ //     },
+ //     body: JSON.stringify({
+ //         name: "Pedro",
+ //         age: "26"
+ //     })
+ // })
+ // .then(response => response.json())//Converte a resposta da requisição para JSON
+// .then(data => {
+// console.log(data) //Exibe os Dados
+//    })
+//.catch(error => {
+//console.log(error, "Erro!") //Exibe erros
+//});
+
+//Async function
+async function getData() {
+    const response = await fetch("https://reqres.in/api/users/")
+    console.log(response.headers);
+
+    const data = await response.json();
+    console.log('Dados dos usuários: ', data)
+
+}   
+
+getData();
